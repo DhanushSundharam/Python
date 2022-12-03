1062e5499f952e0c8967cf6a086f4dd49052ade3
S.Dhanush <118609604+DhanushSundharam@users.noreply.github.com>
2022-12-03 06:46:40 +0530

    Create number-patten-pramid.py

---
+++ Basic/number-patten-pramid.py
@@ -0,0 +1,9 @@
+size = 5
+for i in range(size):
+    # print spaces
+    for j in range(size - i - 1):
+        print(" ", end="")
+    # print numbers
+    for k in range(2 * i + 1):
+        print(k+1, end="")
+    print()
