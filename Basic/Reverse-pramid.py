051743f77059bdf2d8d6748794856183c7a4fe3b
S.Dhanush <118609604+DhanushSundharam@users.noreply.github.com>
2022-12-03 06:47:19 +0530

    Create Reverse-pramid.py

---
+++ Basic/Reverse-pramid.py
@@ -0,0 +1,9 @@
+size = 5
+for i in range(size):
+    # print spaces
+    for j in range(i):
+        print(" ", end="")
+    # print numbers
+    for k in range(2 * (size - i) - 1):
+        print(k+1, end="")
+    print()
