3178cd6dc48c24ed6be40c41dd58fd8ee238809e
S.Dhanush <118609604+DhanushSundharam@users.noreply.github.com>
2022-12-05 21:50:25 +0530

    Create functions.py

---
+++ Basic/functions.py
@@ -0,0 +1,15 @@
+114]: class student:
+     ...:     def getstudent(self,name,age,city):
+     ...:         print("Name",name)
+     ...:         print("Age=",age)
+     ...:         print("City=",city)
+     ...:
+
+In [115]: s=student()
+
+In [116]: student.getstudent(s,"Dhanush",21,"Salem")
+Name Dhanush
+Age= 21
+City= Salem
+
+In [117]:
