ea64b69f88b1f590a965aab489329c601484298c
S.Dhanush <118609604+DhanushSundharam@users.noreply.github.com>
2022-12-08 21:22:58 +0530

    Create oops6.py

---
+++ Basic/oops6.py
@@ -0,0 +1,25 @@
+class Tables:
+    def one(self):
+        for i in range(1,10+1):
+            x=1
+            print(i,"x",x,"=",(i*x))
+    def Two(self):
+        for i in range(1,10+1):
+            x=2
+            print(i,"x",x,"=",(i*x))
+    def Three(self):
+        for i in range(1,10+1):
+            x=3
+            print(i,"x",x,"=",(i*x))
+    def Four(self):
+        for i in range(1,10+1):
+            x=4
+            print(i,"x",x,"=",(i*x))
+    def Five(self):
+        for i in range(1,10+1):
+            x=5
+            print(i,"x",x,"=",(i*x))       
+            
+X=Tables()
+X.one()
+X.Five()
