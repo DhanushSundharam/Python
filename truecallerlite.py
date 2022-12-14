11e6d8fac92cd51bac57cfed89ca98da5c660910
S.Dhanush <118609604+DhanushSundharam@users.noreply.github.com>
2022-12-14 21:49:25 +0530

    Create truecallerlite.py

---
+++ truecallerlite.py
@@ -0,0 +1,54 @@
+class Users:
+    def  aabith(self):
+        print(f"Name : N.Aabith")
+        print(f"Age : 19")
+        print(f"City : salem")
+        
+    def  Arunkar(self):
+        print(f"Name : S.Arunkarthick")
+        print(f"Age : 19")
+        print(f"City : Pennagaram")
+
+    def  Dhanaraj(self):
+        print(f"Name : S.Dhanaraj")
+        print(f"Age : 20")
+        print(f"City : Omalur")
+
+    def Gopi(self):
+        print(f"Name : S.Gopi")
+        print(f"Age : 19")
+        print(f"City : Karumbarai")
+
+    def Gokul(self):
+        print(f"Name : D.Gokul")
+        print(f"Age : 19")
+        print(f"City : Mechari")
+
+    def Gowtham(self):
+        print(f"Name : M.Gowtham")
+        print(f"Age : 19")
+        print(f"City : Salem")
+
+    def  Ruban(self):
+        print(f"Name : A.Rubanchakaravathi")
+        print(f"Age : 19")
+        print(f"City : Salem")   
+
+x=Users()
+y=int(input("Enter a phone Number :"))
+
+if y==9443683982:
+       x.aabith()
+elif y==7604871615:
+       x.Arunkar()
+elif y==8220890689:
+    x.Dhanaraj()
+elif y==8946064191:
+    x.Gokul()
+elif y==8682856821:
+    x.Gopi()
+elif y==9791611924:
+    x.Gowtham
+elif y==9080282414:
+    x.Ruban()
+    
